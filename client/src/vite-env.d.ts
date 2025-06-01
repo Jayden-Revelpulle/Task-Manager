bb7499f7b2b0b0e85ab5cce536ada9922738ebd7
jayrevel2004 <jaydenrevel@gmail.com>
2025-06-01 16:32:18 -0400

    fixed more type errors in client

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // add more env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
