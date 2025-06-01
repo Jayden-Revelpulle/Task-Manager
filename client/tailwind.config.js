d16fc458d696c84faa762ac3618a312557c0a27f
jayrevel2004 <jaydenrevel@gmail.com>
2025-06-01 16:57:04 -0400

    fixed more tailwind errors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,4 +8,8 @@ export default {
     extend: {},
   },
   plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
 } 
