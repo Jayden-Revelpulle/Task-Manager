5d60d677694fb4c15f1f441061158e46f8c3f879
jayrevel2004 <jaydenrevel@gmail.com>
2025-06-01 16:08:17 -0400

    Fix type errors

--- server/db/connect.ts
+++ server/db/connect.ts
@@ -4,12 +4,7 @@ const connectDB = async (url: string): Promise<typeof mongoose> => {
     // Encode the URL to handle special characters
     const encodedUrl: string = encodeURI(url);
     
-    return mongoose.connect(encodedUrl, {
-        writeConcern: {
-            w: 'majority',
-            wtimeoutMS: 2500
-        }
-    });
+    return mongoose.connect(encodedUrl);
 };
 
 export default connectDB; 
